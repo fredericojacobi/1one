1ffb58ec4cd19f0773053508c6df336a1926cc7a
fredericojacobi <fredvjacobi@gmail.com>
2021-08-02 13:05:57 -0300

    Atualizado Constants.php

---
+++ generics/Constants.php
@@ -0,0 +1,23 @@
+<?php
+
+final class Constants
+{
+    const REQUEST_SUCCESS = "Request finalizada com sucesso.";
+    const REQUEST_FIELD_ERROR = "O(s) seguinte(s) campo(s) contém erro(s): ";
+    const REQUEST_NO_BODY = "Body request não encontrado.";
+    const REQUEST_WRONG_BODY = "Body request incorreto.";
+    const REQUEST_ERROR = "Ocorreu um erro ao realizar a request.";
+
+    const SITE_DIR = "./site/";
+    const SITE_HEADER = "/site/src/templates/header.html";
+    const SITE_MENU = "/site/src/templates/menu.html";
+    const SITE_FOOTER = "/site/src/templates/footer.html";
+    const SITE_CSS = "./generics/site.css";
+    const SITE_TEMPLATES = "/site/src/templates/";
+    const API_DIR = "./api/";
+    const API_MODELS = "./api/models/";
+    const API_ENUMS = "./api/models/enums/";
+    const API_CONFIG = "/api/config/";
+
+    const API_USUARIO = "usuario";
+}
